0c7a3393fc9a552511346e8a062c912ac6cb2a4e
ajefferson <ajefferson@acunu.com>
2013-11-08 11:57:00 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 var AA = require("acunu-analytics")
 
 var cluster = new AA.Cluster();
-cluster.addURIs.apply(cluster,['http://ec2-46-137-13-61.eu-west-1.compute.amazonaws.com:8080']);
+cluster.addURIs.apply(cluster,['your acunu analytics server url here:8080']);
 var config = new AA.ConnectionConfig();
 var session = cluster.getSession(config);
 
